3f7a825dba41eec02ba115619fd5361f65c1e4a5
pittpt <97823090+pittpt@users.noreply.github.com>
2022-12-02 21:59:18 +0700

    preload1

--- scroll.js
+++ scroll.js
@@ -3,6 +3,32 @@ const canvas = document.getElementById('scrollAnimation');
 const context = canvas.getContext('2d');
 const btn = document.getElementById('wish');
 
+// https://idea19.netlify.app/img/bg_00033.png
+
+function preloadImages(urls, allImagesLoadedCallback) {
+  var loadedCounter = 0;
+  var toBeLoadedNumber = urls.length;
+  urls.forEach(function (url) {
+    preloadImage(url, function () {
+      loadedCounter++;
+      console.log('Number of loaded images: ' + loadedCounter);
+      if (loadedCounter == toBeLoadedNumber) {
+        allImagesLoadedCallback();
+      }
+    });
+  });
+  function preloadImage(url, anImageLoadedCallback) {
+    var img = new Image();
+    img.onload = anImageLoadedCallback;
+    img.src = url;
+  }
+}
+
+// Let's call it:
+preloadImages('https://idea19.netlify.app/img', function () {
+  console.log('All images were loaded');
+});
+
 const frameCount = 420;
 const currentFrame = (index) =>
   `/img/bg_${index.toString().padStart(5, '0')}.png`;
